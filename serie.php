4803374fd16086ec199c8c30395dfb66c6df5b93
Kristoffer <kristoffer.mn@hotmail.no>
2022-04-01 11:32:56 +0200

    Added serie.php

--- serie.php
+++ serie.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 
-  <link rel="stylesheet" href="index.css">
-
-  <h2>Meny</h2>
+    <h2>Meny</h2>
   <ul>
       <li><a href="serie.php">SERIE </a></li>
       <li><a href="index.php">FILM </a></li>
@@ -19,6 +16,7 @@
 </head>
 <body>
 <?php
+
 // Tilkoblingsinformasjon
 $tjener = "localhost";
 $brukernavn = "root";
@@ -38,10 +36,8 @@ if ($kobling->connect_error) {
 // Angi UTF-8 som tegnesett
 $kobling->set_charset("utf8");
 
-$sql = "SELECT * FROM film JOIN sjanger ON film.idsjanger=sjanger.idsjanger";
+$sql = "SELECT * FROM serie JOIN sjanger ON serie.idsjanger=sjanger.idsjanger";
 $resultat = $kobling->query($sql);
-
-echo "Spørringen $sql ga $resultat->num_rows titler.<br>";
 
 
 echo "<table>";
@@ -53,21 +49,16 @@ echo "<table>";
  <th>LAND</th>
  <th>ÅRSTALL</th>
  <th>SPRÅK</th>
- <th>VARIGHET</th>
  <th>RATING</th>
- <th>AWARDS</th>
-
  </tr>
  ";
 
 
 while($rad = $resultat->fetch_assoc()) {
     $tittel= $rad["tittel"];
-    $varighet = $rad["varighet"];
     $rating = $rad["rating"];
     $årstall = $rad["årstall"];
     $land = $rad["land"];
-    $awards = $rad["awards"];
     $trailer = $rad["trailer"];
     $språk = $rad["språk"];
     $sjanger = $rad["navn"];
@@ -83,9 +74,7 @@ while($rad = $resultat->fetch_assoc()) {
     <td>$land</td>
     <td>$årstall</td>
     <td>$språk</td>
-    <td>$varighet</td>
     <td>$rating</td>
-    <td>$awards</td>
     </tr>
 
    ";
@@ -95,7 +84,6 @@ while($rad = $resultat->fetch_assoc()) {
 
  echo" </table>";
 
-
-?> 
+?>
 </body>
 </html>
